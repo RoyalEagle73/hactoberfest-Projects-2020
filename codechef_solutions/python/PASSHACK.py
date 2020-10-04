cd161d6325071fc4d53f7ec59a24d57806f72baf
MuShaf-NMS <nurmuhammadshafa@gmail.com>
2020-10-04 23:18:58 +0800

    fix PASSHACK.py

--- codechef_solutions/python/PASSHACK.py
+++ codechef_solutions/python/PASSHACK.py
@@ -2,17 +2,8 @@
 a = int(input())
 
 for i in range(a):
-    g = list(map(int,input()))
+    g = input()
     strr = ""
-    if len(g)<10 and len(g)>3:
-        for i in g:
-            if i>1:
-                t = i-2
-                strr += str(t)
-            else:
-                strr += str(i)
-        print(strr)
-    else:
-        for i in g:
-            strr += str(i)
-        print(strr)
+    for j in g:
+        strr += str(int(j)-2)
+    print(strr)
