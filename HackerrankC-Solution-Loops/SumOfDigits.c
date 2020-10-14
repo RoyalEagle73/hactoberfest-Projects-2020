2054bf4973365e0b755253b76a908f6a4dc2e0b6
Parth Pandey <55892170+ParthPandey2236@users.noreply.github.com>
2020-10-15 02:33:13 +0530

    Update SumOfDigits.c

--- HackerrankC-Solution-Loops/SumOfDigits.c
+++ HackerrankC-Solution-Loops/SumOfDigits.c
@@ -1,24 +1,15 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
 
-int main() {
-	
+int main()
+{
     int n;
-    scanf("%d", &n);
-    
-    int a = n%10;
-    n= n/10;
-    int b = n%10;
-    n= n/10;
-    int c = n%10;
-    n= n/10;
-    int d = n%10;
-    n= n/10;
-    int e = n%10;
-    n= n/10;
-    int f = a+b+c+d+e;
-    printf("%d", f);
+    int sum=0;
+    scanf("%d",&n);
+    while(n!=0){
+        sum+=n%10;
+        n=n/10;
+    }
+    printf("%d",sum);
+
     return 0;
 }
