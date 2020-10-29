096df8150dd5fc8bf07e586b1a95513aecbfef41
Gabriel Moura Vargas <gab.mv@hotmail.com>
2020-10-28 23:32:49 -0300

    Delete removeDuplicate.py

--- removeDuplicate.py
+++
@@ -1,8 +0,0 @@
-# This program is used to remove duplicated values from a list passed by parameter
-
-def compare(list):
-    newList = []
-    for x in list:
-        if x not in newList:
-            newList.append(x)    
-    return newList
